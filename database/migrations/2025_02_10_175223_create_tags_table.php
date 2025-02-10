fb10905252470faed152805a08fe218d6a8c0608
frogel <franrogel2003@gmail.com>
2025-02-10 18:09:53 +0000

    modelos y controladores tag y tarea

---
+++ database/migrations/2025_02_10_175223_create_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Tag;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->string('descripcion');
+            $table->date('fecha_comienzo');
+            $table->date('fecha_final');
+            $table->foreignIdFor(Tag::class)
+              ->constrained()
+              ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
